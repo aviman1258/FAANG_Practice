0cd5381aa58b8a4e63d2f1e4cd46604bb590a7d6
Avishek Chandra <achandra1258@gmail.com>
2020-06-14 23:33:56 -0700

    extratced Node to class file and added inirder successor

---
+++ AmazonPracticeProblems/BinaryTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public partial class BinaryTree
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+    }
+}
