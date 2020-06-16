e0f28a1d29d6780a956d14ac889cc909e262f849
Avishek Chandra <achandra1258@gmail.com>
2020-06-16 12:14:52 -0700

    Adding new project

--- AmazonPracticeProblems/LambaExpressions/Program.cs
+++ AmazonPracticeProblems/LambaExpressions/Program.cs
@@ -8,8 +8,65 @@ namespace LambaExpressions
 {
     class Program
     {
+        //delegate double CalAreaPointer(int r);
+        //static CalAreaPointer cpointer = CalculateArea;
         static void Main(string[] args)
         {
+            //double area = cpointer.Invoke(20);
+
+            //Anonymous function lets you write the function
+            //inline and reduce code.
+
+            //CalAreaPointer cpointer = new CalAreaPointer(
+            //    delegate (int r)
+            //    {
+            //        return 3.14 * r * r;
+            //    }
+            //    );
+
+            //double area = cpointer(20);
+
+
+            //Lamba expression minimizes the lines of code
+
+            //CalAreaPointer cpointer = r => 3.14 * r * r;
+
+            //We can minimize lines of code even more
+            //Func is a generic delegate:
+            //Takes in Double and outputs a double
+
+            Func<double, double> cpointer = r => 3.14 * r * r;
+
+            double area = cpointer(20);
+
+            //Action delegates have an input but void output
+            Action<string> myAction = y => Console.Write(y);
+            myAction("Hello");
+
+            //Predicate can take any input and will
+            //output a boolean type
+
+            Predicate<string> CheckGraeterThan5 = x => x.Length > 5;
+            Console.WriteLine(CheckGraeterThan5("Avishek"));
+
+            List<string> oString = new List<string>();
+
+            oString.Add("Duke");
+            oString.Add("Avishek");
+
+            //You can pass a predicate into the Find function
+            //You will find strings that match that predicate
+            //or strings with length greater than 5
+            string str = oString.Find(CheckGraeterThan5);
+            Console.Write(str);
+
+            //Expression trees
+            
         }
+
+        //static double CalculateArea(int r)
+        //{
+        //    return 3.14 * r * r;
+        //}
     }
 }
