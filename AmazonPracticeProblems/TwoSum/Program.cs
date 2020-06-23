dd72cc5da351b5e43212976a6de5e8d3e272d7a4
Avishek Chandra <achandra1258@gmail.com>
2020-06-22 21:22:49 -0700

    Tweaked Algorithm

--- AmazonPracticeProblems/TwoSum/Program.cs
+++ AmazonPracticeProblems/TwoSum/Program.cs
@@ -12,19 +12,30 @@ namespace TwoSum
         {
             //Given an array of integers,
             //return indices of the two numbers such that
-            //they add up tp specific target.
+            //they add up to specific target.
 
             //You may assume that each input
             //would have exactly one solution,
             //and you may noy use the same element twice.
 
 
-            int[] nums = { 2, 7, 11, 15 };
-            int target = 13;
+            int[] nums = { 
+                230, 863, 916, 585, 981, 404, 316, 785, 88, 12,
+                70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 
+                325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 
+                168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 
+                863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 
+                314, 422, 927, 783, 930, 282, 306, 506, 44, 926,
+                691, 568, 68, 730, 933, 737, 531, 180, 414, 751,
+                28, 546, 60, 371, 493, 370, 527, 387, 43, 541,
+                13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 
+                538, 427, 583, 368, 375, 173, 809, 896, 370, 789 
+            };
+            int target = 542;
 
             int[] indiciesOfSum = TwoSum(nums, target);
 
-            Console.WriteLine("Expecting: [ 0 , 2 ].");
+            //Console.WriteLine("Expecting: [ 0 , 2 ].");
 
             Console.Write("Returned: [ ");
 
@@ -76,7 +87,8 @@ namespace TwoSum
                     return new int[] { i , index };
                 }
 
-                dictionary.Add(nums[i], i);
+                if(!dictionary.ContainsKey(nums[i]))
+                    dictionary.Add(nums[i], i);
             }
 
             throw new ArgumentNullException("No two sum solution.");
