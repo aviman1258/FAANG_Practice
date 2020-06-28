203285898c83b6dedb3804475893ef17db92af48
Avishek Chandra <achandra1258@gmail.com>
2020-06-27 19:36:10 -0700

    adding new proj

---
+++ AmazonPracticeProblems/LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestPalindromicSubstring
+{
+    class Program
+    {
+        //Given a string s, find the longest palindromic substring in s.
+
+        static void Main(string[] args)
+        {
+            string s = "bb";
+
+            string palindrom = LongestPalindrom(s);
+
+            Console.WriteLine(palindrom);
+        }
+
+        private static string LongestPalindrom(string s)
+        {
+           //base cases
+            if (s == null || s.Length < 1) return "";
+
+            int start = 0;
+            int end = 0;
+
+            //loop through the letters of the string
+            for(int i = 0; i < s.Length; i++)
+            {
+                //dealing with racecar
+                int len1 = LengthOfPalindromInSubstring(s, i, i);
+
+                //dealing with abba
+                int len2 = LengthOfPalindromInSubstring(s, i, i + 1);
+
+                int len = Math.Max(len1, len2);
+
+                if(len - 1> end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return s.Substring(start, end + 1);
+        }
+
+        //Find palindrom length in substring
+        private static int LengthOfPalindromInSubstring(string s, int left, int right)
+        {
+            //as long as left and right are in range and the two letters 
+            //being compared are same.
+            while(left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
+        }
+    }
+}
